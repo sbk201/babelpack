ed4c9d5fd3169c0e7d17a8c8be0823bb9c4a305c
jsiu <john,siu@cookmedical.com>
2018-02-05 11:38:07 +0800

    init

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// XMLHttpRequest wrapper using callbacks
+function request(obj, successHandler, errorHandler) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(obj.method || "GET", obj.url);
+    if (obj.headers) {
+        Object.keys(obj.headers).forEach(function(key) {
+            xhr.setRequestHeader(key, obj.headers[key]);
+        });
+    }
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            successHandler(xhr.response);
+        } else {
+            errorHandler(xhr.statusText);
+        }
+    };
+    xhr.onerror = function() {
+        errorHandler(xhr.statusText);
+    };
+    xhr.send(obj.body);
+}
+tg
+request({url:"employees.json"},
+    function(data) {
+        let employees = JSON.parse(data);
+        let html = "";
+        employees.forEach(function(employee){
+            html += "<div><img src='" + employee.picture + "'/><div>" + employee.firstName + " " + employee.lastName + "<p>" + employee.phone + "</p></div></div>";
+        });
+        document.getElementById("list").innerHTML = html;
+    },
+    function(error) {
+        console.log(error);
+    }
+);
